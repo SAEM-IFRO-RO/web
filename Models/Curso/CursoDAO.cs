08adffd2c796bbb5e01bc170f88260ce614d397c
Giovanna Rodrigues <escolargiovanna@gmail.com>
2025-10-09 09:33:41 -0400

    Listagem de curso no CursoDAO

--- Models/Curso/CursoDAO.cs
+++ Models/Curso/CursoDAO.cs
@@ -12,7 +12,27 @@ namespace SaemWeb.Models
       _Connection = Connection;
     }
 
+    // Inserção
+    public void Inserir(Curso curso)
+    {
+      try
+      {
+        var comando = _Connection.CreateCommand(" INSERT INTO curso VALUES (null, @_nome, @_dificuldade, @_descricao, @_id_inst_fk)");
 
+        comando.Parameters.AddWithValue("@_nome", curso.Nome);
+        comando.Parameters.AddWithValue("@_dificuldade", curso.Dificuldade);
+        comando.Parameters.AddWithValue("@_descricao", curso.Descricao);
+        comando.Parameters.AddWithValue("@_id_inst_fk", curso.IdInstrumentoFk);
+
+        comando.ExecuteNonQuery();
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+
+    // Listar
     public List<Curso> Listar()
     {
       var lista = new List<Curso>();
